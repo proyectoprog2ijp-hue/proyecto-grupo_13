9a20394f80b6cff24f28775df653a37bc8ce0e51
ignacioiramas157 <ignacio.iramas157@gmail.com>
2026-06-11 02:29:05 +0000

    lectura de datos

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import codecs;
 import csv
+import streamlit as st
+
+archivo = open("establecimientos-educativos-12K.csv", newline='')
+leectura_archivo = csv.DictReader(archivo)
 
 def read():
-    archivo = open("establecimientos-educativos-12K.csv", newline='')
-    for i in archivo:
-            print("".join(i))
-
+    for categoria in leectura_archivo:
+        return(categoria['establecimiento_nombre'])
 
 
 def main():
