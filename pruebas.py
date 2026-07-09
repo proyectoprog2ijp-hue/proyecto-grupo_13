2d38dbb652143f1d18fb401f545a17fdf26d4270
Matias642312 <84029179+Matias642312@users.noreply.github.com>
2026-07-09 05:07:32 +0000

    Optimización

--- pruebas.py
+++
@@ -1,8 +0,0 @@
-import programa
-
-def main():
-    #print(programa.contador_ambito(programa.apertura_archivo("test_datos.csv")))
-    #print(programa.elecciones_modalidades(programa.apertura_archivo("test_datos.csv")))
-    #print(programa.modalidad("Educacion Común", programa.apertura_archivo("test_datos.csv")))
-    print(programa.modalidad("Educacion Artistica", programa.apertura_archivo("test_datos.csv")))
-main()
